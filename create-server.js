7d11ea4d5e1d9d0a897d1dc7791d0fa85bbf20d8
todmy <admin@todmy.local>
2020-03-22 14:41:23 +0200

    feat: added ability to read body from request
    In scope of it also added helmet for security reasons

--- create-server.js
+++ create-server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(helmet());
+app.use(bodyParser.urlencoded());
+
 const port = process.env.SERVER_PORT;
 
 module.exports = function serverBuilder(routes) {
